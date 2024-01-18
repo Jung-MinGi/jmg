6aad850521f69636b486bdccde5b0f69ebe8645e
jung <mg1997@naver.com>
2024-01-18 21:37:48 +0900

    annotation add

--- src/main/java/com/blog/jmg/repository/mapper/DataGetMapper.java
+++ src/main/java/com/blog/jmg/repository/mapper/DataGetMapper.java
@@ -4,10 +4,12 @@ import com.blog.jmg.domain.FindTextParamDTO;
 import com.blog.jmg.domain.WriteForm;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Mapper
+@Repository
 public interface DataGetMapper {
     List<String> getTablesName();
     WriteForm findTextByTitle(FindTextParamDTO findTextParamDTO);
