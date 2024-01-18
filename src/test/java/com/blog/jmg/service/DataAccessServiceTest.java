f2bbcd44d5257bf34b18e7fada035a860b13fce4
jung <mg1997@naver.com>
2024-01-18 14:34:59 +0900

    testcode update

--- src/test/java/com/blog/jmg/service/DataAccessServiceTest.java
+++ src/test/java/com/blog/jmg/service/DataAccessServiceTest.java
@@ -1,29 +1,12 @@
 package com.blog.jmg.service;
 
-import com.blog.jmg.domain.service.TextDataAccessService;
-import com.blog.jmg.repository.mapper.DataGetMapper;
-import com.blog.jmg.repository.mybatis.DataGetRepositoryImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-@MybatisTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Slf4j
 class DataAccessServiceTest {
-    @Autowired
-    private DataGetMapper mapper;
 
     @Test
     void test() {
-        TextDataAccessService service = new TextDataAccessService(new DataGetRepositoryImpl(mapper));
-        List<String> tables = service.getTables();
-        assertThat(tables.size()).isNotZero();
     }
 }
